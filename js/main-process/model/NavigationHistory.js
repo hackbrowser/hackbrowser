a7abd5c46cbb94a7047526f6c55da077bea76245
subwaymatch <subwaymatch@gmail.com>
2017-03-13 20:15:19 -0400

    Remove duplicate entries from autocomplete queries

--- js/main-process/model/NavigationHistory.js
+++ js/main-process/model/NavigationHistory.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const Datastore = require('nedb');
-const db = new Datastore({ filename: global.__app.dataPath + '/navigation-history.db', autoload: true });
+const path = require('path'); 
+const navigationHistoryDb = new Datastore({ 
+	filename: path.join(global.__app.dataPath, 'navigation-history.db'), 
+	autoload: true 
+});
+const autoCompleteDb = new Datastore({ 
+	filename: path.join(global.__app.dataPath, 'auto-complete-entries.db'), 
+	autoload: true 
+}); 
+
+const logger = global.__app.logger; 
 
 var NavigationHistory = {};
 
@@ -9,13 +19,43 @@ NavigationHistory.addNavigationHistory = function(navigationInfo, callback) {
 	// add navigation history timestamp
 	navigationInfo.date = new Date();
 
-	db.insert(navigationInfo, function(err, newDoc) {
+	let completeCount = 0; 
+	let jobCount = 2; 
+
+	navigationHistoryDb.insert(navigationInfo, function(err, newDoc) {
 		if (err) {
 			callback(err);
 		} else {
-			callback();
+			completeCount++; 
+			if (completeCount >= jobCount) {
+				callback(); 
+			}
 		}
 	});
+
+	autoCompleteDb.update({
+		url: navigationInfo.url
+	}, 
+	{
+		$inc: { visitCount: 1 }, 
+		$set: {
+			date: navigationInfo.date, 
+			title: navigationInfo.title
+		}
+	}, 
+	{
+		upsert: true
+	}, 
+	function(err, numAffected, affectedDocuments, upsert) {
+		if (err) {
+			callback(err); 
+		} else {
+			completeCount++; 
+			if (completeCount >= jobCount) {
+				callback(); 
+			}
+		}
+	}); 
 };
 
 /**
@@ -24,16 +64,23 @@ NavigationHistory.addNavigationHistory = function(navigationInfo, callback) {
  * @param callback function to be called when all documents are removed (function(err, numRemoved))
  */
 NavigationHistory.clearNavigationHistory = function(callback) {
-	db.remove({}, callback);
+	navigationHistoryDb.remove({}, callback);
 };
 
 NavigationHistory.getAutoCompleteList = function(searchTerm, callback) {
+	// Escape RegEx characters inside search term
+	searchTerm = searchTerm.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 	var searchRegEx = new RegExp(searchTerm);
 
-	db.find({
+	// Find a matching domain
+	autoCompleteDb.find({
 		url: searchRegEx
 	})
 		.limit(10)
+		.sort({ 
+			visitCount: -1, 
+			date: -1
+		})
 		.exec(function(err, autoCompleteEntries) {
 			callback(autoCompleteEntries);
 		});
